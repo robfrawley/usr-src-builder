413257a32273d480dd5872217272144177deda62
Rob Frawley 2nd <rmf@src.run>
2017-05-25 17:34:31 -0400

    add composer removal step

---
+++ _env-cleanup/composer-del.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+##
+# This file is part of `src-run/usr-src-builder`
+#
+# (c) Rob Frawley 2nd <rmf@scr.be>
+#
+# For the full copyright and license information, view the LICENSE.md
+# file distributed with this source code.
+##
+
+RT_COMMANDS_ACT=(
+	"rm -fr ${DIR_CWD}/composer"
+)
+
+# EOF #
